ba6e3450b6d749c44d4ed60a5d4e1c671ae1e535
ar313 <43295107+ar313@users.noreply.github.com>
2019-05-04 20:30:08 +0300

    class Music -added v1.0

---
+++ app/src/main/java/com/example/dtr313/musiclibrary/music.java
@@ -0,0 +1,62 @@
+package com.example.dtr313.musiclibrary;
+
+public class music {
+
+    private String mName;
+    private String mArtist;
+    private String mAlbum;
+    private int duration;
+    private String description;
+
+    music(){}
+
+    music(String mName, String mArtist, String mAlbum, int duration, String description) {
+
+        this.mName = mName;
+        this.mArtist = mArtist;
+        this.mAlbum = mAlbum;
+        this.duration = duration;
+        this.description = description;
+    }
+
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String mName) {
+        this.mName = mName;
+    }
+
+    public String getArtist() {
+        return mArtist;
+    }
+
+    public void setArtist(String mArtist) {
+        this.mArtist = mArtist;
+    }
+
+    public String getAlbum() {
+        return mAlbum;
+    }
+
+    public void setAlbum(String mAlbum) {
+        this.mAlbum = mAlbum;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
